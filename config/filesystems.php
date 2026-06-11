ec343962f6b7ccf217bf1f59eed8e5e79ecd8c89
Arf-7447 <arippandu55@gmail.com>
2026-06-11 21:56:59 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,4 +77,12 @@ return [
         public_path('storage') => storage_path('app/public'),
     ],
 
+    'gcs' => [
+        'driver' => 'gcs',
+        'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
+        'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
+        'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', ''),
+        'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null),
+        'visibility' => 'public',
+    ],
 ];
