59b0902cf26c6af1f8753d1569c51062671fcd13
Arf-7447 <arippandu55@gmail.com>
2026-06-11 15:48:43 +0700

    update files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,9 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\HandleAppearance;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,34 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        $middleware->encryptCookies(except: ['appearence', 'sidebar_state']);
-
-        $middleware->web(append:[
-            HandleAppearance::class,
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
-
-        $middleware->trustProxies(
-            '*',
-            Request :: HEADER_X_FORWARDED_FOR |
-            Request :: HEADER_X_FORWARDED_HOST |
-            Request :: HEADER_X_FORWARDED_PORT |
-            Request :: HEADER_X_FORWARDED_PROTO |
-
-        );
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
-    // class TrustProxies extends Middleware
-    // {
-    //     protected $proxies = '*';
-    //     protected $headers =
-    //     Request::HEADER_X_FORWARDED_FOR |
-    //     Request::HEADER_X_FORWARDED_HOST |
-    //     Request::HEADER_X_FORWARDED_PORT |
-    //     Request::HEADER_X_FORWARDED_PROTO;
-    // }
