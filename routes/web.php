f64bd4d87b0e970cc09236b34402e32435085a4f
Arf-7447 <arippandu55@gmail.com>
2026-07-07 09:14:19 +0700

    debug error file upload

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,11 @@ Route::post('/img-classify', [PredictController::class, 'predict'
 Route::get('/img-classify/result', [PredictController::class, 'result'
     ])->name('img-classify.result');
 });
+Route::get('/phpinfo-upload', function () {
+    return [
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+        'memory_limit' => ini_get('memory_limit'),
+        'max_file_uploads' => ini_get('max_file_uploads'),
+    ];
+});
